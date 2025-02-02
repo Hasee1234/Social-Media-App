95c0ae9943bad7ef9ab73ba57e2171b8148b738c
hasee <haseemirza123@gmail.com>
2025-02-02 20:49:20 +0500

    done

--- Social-Media-App/src/Pages/Signup/Signup.jsx
+++ Social-Media-App/src/Pages/Signup/Signup.jsx
@@ -29,14 +29,19 @@ export default function Signup() {
   return (
     <div>
         <h1>Signup</h1>
+        <label >Enter your email:</label>
         <input type="text" placeholder="email" onChange={(e)=> setEmail(e.target.value)}/>
         <br/>
+        <label >Enter password:</label>
         <input type="password" placeholder="password" onChange={(e)=> setPassword(e.target.value)}/>
         <br/>
+        <label >Enter your name:</label>
         <input type="text" placeholder="name" onChange={(e)=> setName(e.target.value)}/>
         <br/>
+        <label >Enter your phone No.</label>
         <input type="text" placeholder="phone" onChange={(e)=> setPhone(e.target.value)}/>
         <br/>
+        <label>Enter your address</label>
         <input type="text" placeholder="address" onChange={(e)=> setAddress(e.target.value)}/>
         <br/>
         <label>Male</label>
